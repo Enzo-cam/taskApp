9d32547180754eef86c525c732a6a572d34ad4a3
EnzoCam7 <camerac09@gmail.com>
2023-05-15 20:23:10 -0300

    Configuration

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+
+// Decorador
+@Module({
+  // Añadir funcionalidades extra
+  imports: [],
+  // Rutas que tendrá este modulo
+  controllers: [],
+  // Funciones encargadas de comunicarse con la BDD y reutilizables
+  providers: [],
+})
+export class AppModule {}
