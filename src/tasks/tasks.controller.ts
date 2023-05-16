9632b8061ffa1362e87c2e193fc12a0b1fd73aa5
EnzoCam7 <camerac09@gmail.com>
2023-05-15 21:21:26 -0300

    Controllers declarations

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,11 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Body, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
 
 // Cuando se solicite lo que va entre '', ejecutará con los metodos declarados en esta clase
 @Controller('tasks')
 export class TasksController {
-  
+  constructor(private tasksService: TasksService) {}
+
   @Get()
-  helloworld() {
-    return 'Hello gente';
+  getAllTasks() {
+    return this.tasksService.getAllTasks();
+  }
+
+  @Post()
+  createTask(@Body() newTask: any) {
+    console.log(newTask);
+    return 'Probando post';
+    // return this.tasksService.createTask()
   }
 }
