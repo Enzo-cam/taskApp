5f4b271723dcd266cde848defa6a33083d9f1aad
EnzoCam7 <camerac09@gmail.com>
2023-05-15 21:21:39 -0300

    Services declarations

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,5 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.entity';
+import { v4 } from 'uuid';
 
 // Injectable pq se inyecta en otras partes.
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  //Se declara private para que solo se pueda acceder en esta clase.
+  private tasks: Task[] = [
+    {
+      id: '1',
+      title: 'First task',
+      description: 'First task of nest',
+      status: TaskStatus.PENDING,
+    },
+  ];
+
+  getAllTasks() {
+    return this.tasks;
+  }
+
+  createTask(title: string, description: string) {
+    const newTask = {
+      id: v4(),
+      title,
+      description,
+      status: TaskStatus.PENDING,
+    };
+
+    this.tasks.push(newTask);
+    return newTask;
+  }
+
+  updateTask() {}
+  deleteTask() {}
+}
