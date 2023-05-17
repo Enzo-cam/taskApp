72c9a5db74addff595a041206883fbd10b1096ac
EnzoCam7 <camerac09@gmail.com>
2023-05-17 01:18:13 -0300

    Validators and Pipes

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   // Carga todos los modulos
   const app = await NestFactory.create(AppModule);
+
+  // Esta linea aclara que cuando YO cree validaciones deben validarse siempre.
+  app.useGlobalPipes(new ValidationPipe)
   await app.listen(3000);
 }
 bootstrap();
--- src/tasks/dto/task.dto.ts
+++ src/tasks/dto/task.dto.ts
@@ -1,12 +1,29 @@
 import { TaskStatus } from '../task.entity';
+import { IsNotEmpty, IsString, MinLength, IsOptional, IsIn } from 'class-validator';
 
 export class CreateTaskDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
   title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
   description: string;
 }
 
 export class UpdateTaskDto {
+  @IsString()
+  @IsOptional()
   title?: string;
+
+  @IsString()
+  @IsOptional()
   description?: string;
+
+  @IsString()
+  @IsOptional()
+  @IsIn([TaskStatus.PENDING, TaskStatus.DONE, TaskStatus.IN_PROGRESS])
   status?: TaskStatus;
 }
